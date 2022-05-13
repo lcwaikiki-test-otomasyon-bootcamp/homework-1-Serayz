0e269a9911b5a45ae35aa1497da57953d7b1c4cd
Serayz <ezgiserayb@gmail.com>
2022-05-13 23:42:11 +0300

    odev1

---
+++ src/main/java/Main.java
@@ -0,0 +1,55 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.Console;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications","--disable-blink-features=AutomationControlled");
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver(
+                options
+        );
+
+        driver.get("https://www.lcwaikiki.com/");
+        driver.manage().timeouts().pageLoadTimeout(100, TimeUnit.SECONDS);
+
+        WebElement girisYap = driver.findElement(By.xpath("//*[@id=\"header__container\"]/header/div[2]/div[3]/div/span/div/a"));
+        girisYap.click();
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement elementEposta = driver.findElement(By.name("email"));
+        elementEposta.click();
+        elementEposta.sendKeys("hyd@hyd.com");
+        WebElement elementSifre = driver.findElement(By.name("password"));
+        elementSifre.click();
+        elementSifre.sendKeys("951951");
+
+        try {
+            Thread.sleep(400);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        WebElement elementGirisYap = driver.findElement(By.xpath("//*[@id=\"login\"]/div/div/div/div/form/button"));
+        elementGirisYap.click();
+
+        WebElement elementError = driver.findElement(By.xpath("//*[@id=\"login\"]/div/div/div/div/div/p"));
+        elementError.isDisplayed();
+
+    }
+    }
+
+
+
